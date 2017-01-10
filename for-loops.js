677bb809426fb09b0922eb4769f5a684ff67995f
barfootaaron <barfootaaron@yahoo.com>
2017-01-10 13:04:02 -0600

    completed js loop practice

---
+++ for-loops.js
@@ -0,0 +1,58 @@
+
+	// Increment by 10 //
+
+//		for (var i=5; i<=120; i+=5) {
+//			console.log("Current value is " + i );
+//		}
+
+
+
+
+	// Decrement by division //
+
+//		for (var i=4096; i>=1; i/=2) {
+//			console.log("Current value is " + i );
+//		}
+
+
+
+
+	// Array iteration // 
+
+//	var presidents = 	['George Washington',
+//						'John Adams',
+//						'Thomas Jefferson',
+//						'James Madison',
+//						'James Monroe',
+//						'John Quincy Adams',
+//						'Andrew Jackson',
+//						'Martin Van Buren'];
+
+//	for (var i=0; i<presidents.length; i+=1) {
+//		console.log('President #' + (i+1) + ' was ' + presidents[i]);
+//	}	
+
+
+
+		
+	// Object iteration //
+
+var antSpecies = {
+	argentine: {},
+	army: {},
+	bigHeaded: {},
+	black: {},
+	bull: {},
+	carpenter: {},
+	crazy: {},
+	fire: {},
+	glider: {},
+	honeyPot: {},
+	jackJumper: {}
+}
+
+for ( var key in antSpecies) {
+	console.log(key);
+}
+
+		
